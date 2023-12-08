3e59a86bfcbab3a4ccf71e91eab089df46e7f396
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2023-12-08 20:04:52 -0300

    Cria as funções de finalizar o jogo

--- src/funcoesPadroes.js
+++ src/funcoesPadroes.js
@@ -32,15 +32,35 @@ export function escolherColunaAleatoria(){
 
 // Função de testar se podemos colocar na coluna
 export function colocaDadoNaColuna(coluna, dado){
-    if(coluna == 1 && ratao.col1.length < 3){
-        col1.push(dado)
-    } else if(coluna == 2 && ratao.col2.length < 3){
-        col2.push(dado)
-    } else if(coluna == 3 && ratao.col3.length < 3){
-        col3.push(dado)
+    if(! acabouJogo()){
+        if(coluna == 1 && ratao.col1.length < 3){
+            col1.push(dado)
+        } else if(coluna == 2 && ratao.col2.length < 3){
+            col2.push(dado)
+        } else if(coluna == 3 && ratao.col3.length < 3){
+            col3.push(dado)
+        } else {
+            coluna = escolherColunaAleatoria()
+            colocaDadoNaColuna(coluna, dado)
+        }
     } else {
-        coluna = escolherColunaAleatoria()
-        colocaDadoNaColuna(coluna, dado)
+        finalizaJogo(calculaPontuacao)
     }
 }
 
+// função que diz se o jogo acabou
+export function acabouJogo(){
+    if((lamb.col1.length == 3 &&
+        lamb.col2.length == 3 &&
+        lamb.col3.length == 3) ||
+        (ratao.col1.length == 3 &&
+        ratao.col2.length == 3 &&
+        ratao.col3.length == 3)) {
+            return true
+        } else {
+            return false
+        }
+}
+
+// função que de fato termina o jogo
+finalizaJogo(ganhador)
