8e3cf15d63442b19eab33c15e4a80cf7d8310a5d
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2023-12-13 14:31:21 -0300

    Atualiza o nome da função

--- src/ratao.js
+++ src/ratao.js
@@ -3,7 +3,7 @@ import { getRatao, jogarDado, colocaDadoNaColuna, atualizaPontuacao } from "./fu
 let dado, coluna, pontuacao
 
 // Função de escolher coluna aleatoriamente do Ratao
-export function escolherColunaAleatoria(){
+export function escolheColunaAleatoria(){
     coluna = Math.floor(Math.random() * 3) + 1
     return coluna
 }
