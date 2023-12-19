bb28c5c7a16ba1fb037e6549ceffa703ee2da1c7
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2023-12-19 17:34:20 -0300

    Adiciona comentários no lamb

--- src/lamb.js
+++ src/lamb.js
@@ -1,4 +1,4 @@
-// falta adicionar comentários
+// cria o objeto lamb
 let lamb = {
   colunas: {
     col1: [],
@@ -13,24 +13,33 @@ let lamb = {
   pontuacaoTotal: 0,
 };
 
+// Seleciona as 3 colunas do rato vindas do html
 const col1C = document.querySelector("#col1C");
 const col2C = document.querySelector("#col2C");
 const col3C = document.querySelector("#col3C");
 
+// Seleciona os 3 espaços para colocarmos as pontuações referentes a cada coluna vindo do html
 const pont1C = document.querySelector("#pont1C");
 const pont2C = document.querySelector("#pont2C");
 const pont3C = document.querySelector("#pont3C");
 
+// Seleciona o espaço para colocarmos a pontuação final vindo do html
 const ptC = document.querySelector("#pontuacaoC");
 
+// Nessa função damos acesso direto ao objeto ratao à todos os arquivos que importarem essa função
 export function getLamb() {
+  // optamos por enviarmos o objeto e não um structuredClone para que a atualização do mesmo fosse mais fácil
   return lamb;
 }
 
+// Essa função tem como objetivo atualizar as colunas do carneiro (todas as colunas ao mesmo tempo)
 export function atualizaColunaC() {
+  // primeiro esvaziamos seu conteúdo
   col1C.innerHTML = "";
   col2C.innerHTML = "";
   col3C.innerHTML = "";
+
+  // depois utilizamos um for para percorrer cada coluna dentro do objeto e inserir os valores no html
   for (let i = 0; i < lamb.colunas.col1.length; i++) {
     col1C.innerHTML += `<span> ${lamb.colunas.col1[i]} </span>`;
   }
@@ -42,6 +51,7 @@ export function atualizaColunaC() {
   }
 }
 
+// Essa função coloca em cada espaço para pontuação vindo do html a pontuação das colunas
 export function mostraPontoColunaC(coluna) {
   switch (coluna) {
     case 1:
@@ -56,6 +66,7 @@ export function mostraPontoColunaC(coluna) {
   }
 }
 
+// Essa função atualiza a pontuação geral dentro do objeto e atualiza a pontuação no html
 export function atualizaPontuacaoGeralC() {
   let caminho = lamb.pontuacoes;
   lamb.pontuacaoTotal = 0;
